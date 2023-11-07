26053a20f81a2aa963dc709518b4818993219882
Génesis Baquerizo <gdbaquer@espol.edu.ec>
2023-11-07 11:50:02 -0500

    Cambios en clases y metodos

--- sistema_biblioteca.py
+++ sistema_biblioteca.py
@@ -1,49 +1,108 @@
 import os
 from usuario import Usuario
 from libro import Libro
-
+from datetime import datetime,timedelta
 class Sistema_Biblioteca:
     
     
-        def __init__(self):
-                self.libros_inventario = {}
+    def __init__(self):
+                self.libros_inventario = {
+                    'codigo': "199",
+                    'titulo': '100 años de soledad',
+                    'autor': 'Garcia Marquez',
+                }
 
 
-def iniciar_sesion():
-        usuario = input("Ingrese su nombre de usuario: ")
-        contrasena = input("Ingrese su contraseña: ")
-        
-        if usuario == "admin" and contrasena == "12345":
-            usuario= Usuario("administrador")
-            print("Este usuario es administrador")
-        elif usuario == "est" and contrasena == "54321":
-            usuario= Usuario("estudiante")
-            print("Este usuario es estudiante")
-        return usuario
-    
-      
-
-
-def main():
-        print("BIENVENIDO")
-        
-        usuario_autenticado=iniciar_sesion()
-        if usuario_autenticado.rol=="administrador":             usuario_autenticado.menu_adm()
-        elif usuario_autenticado.rol=="estudiante":
-             usuario_autenticado.menu_estudiante()   
-        else:
-            print("Usuario o clave incorrecto")
-
-        
+    def iniciar_sesion(self):
+        while True:
+            usuario = input("Ingrese su nombre de usuario: ")
+            contrasena = input("Ingrese su contraseña: ")
             
+            if usuario == "admin" and contrasena == "12345":
+                usuario= Usuario("administrador")
+                print("Este usuario es administrador")
+            elif usuario == "est" and contrasena == "54321":
+                usuario= Usuario("estudiante")
+                print("Este usuario es estudiante")
+            else:
+                usuario= Usuario("")
+                print("ingrese usuario y contraseña válidos")
+            return usuario
+            
+            
+    def menu_adm(self, usuario):
+            while True:
+                print("Elija la opcion que desee ejecutar")
+                print("\n1) Agregar libro a inventario")
+                print("2) Buscar libro")
+                print("3) Regresar a iniciar sesion")
+                opcion = input("Seleccione una opción (1, 2) o 'q' para salir: ")
+                
+                if opcion=="1":
+                    print("Ingrese codigo del libro ")
+                    codigo= str(input(''))
+                    print("Ingrese titulo del libro ")
+                    titulo= str(input(''))
+                    print("Ingrese autor del libro ")
+                    autor= str(input(''))
+                    libro= Libro(codigo, titulo, autor)
+                    usuario.agregar_libro(libro)
+                
+                elif opcion=="2":
+                    print("Ingrese codigo del libro a buscar")
+                    codigo= str(input(''))
+                    usuario.buscar_libro(libro.codigo)
+                    
+                elif opcion=="3":
+                    usuario= Usuario("")
+                    self.iniciar_sesion()
+                
+                elif opcion.lower() == 'q':
+                    break                
+                else:
+                    print("Elija una opcion valida") 
+
+    def menu_estudiante(self,usuario):
+            self.usuario=usuario
+            while True:
+                print("\n1. Ver libros disponibles")
+                print("2. Ver libros prestados")
+                print("3. Hacer préstamo")
+                print("4) Regresar a iniciar sesion")
+                opcion = input("Seleccione una opción (1,2,3,4) o 'q' para salir del programa: ")
+
+                if opcion == '1':
+                    usuario.ver_libros_disponibles(self.libros_inventario)
+                elif opcion == '2':
+                    usuario.ver_libros_prestados()
+                elif opcion == '3':
+                    codigo_libro = input("Ingrese el código del libro que desea prestar: ")
+                    fecha_prestamo = datetime.now()
+                    usuario.hacer_prestamo(codigo_libro, fecha_prestamo, self.libros_inventario)
+                elif opcion == '4':
+                    self.iniciar_sesion()
+                elif opcion.lower() == 'q':
+                    break
+                else:
+                    print("Opción inválida. Intente de nuevo.")
+
+    def main(self):
+            print("BIENVENIDO")
         
-        
-        
-        
-        
+            usuario_autenticado=self.iniciar_sesion()
+            if usuario_autenticado.rol=="administrador":             
+                self.menu_adm(usuario_autenticado)
+            elif usuario_autenticado.rol=="estudiante":
+                self.menu_estudiante(usuario_autenticado)   
+            else:
+                print("Usuario o clave incorrecto")
+
+
    
 if __name__ == "__main__":
-    main()
+    sistema = Sistema_Biblioteca()
+    sistema.main()
+      
 
         
         
--- usuario.py
+++ usuario.py
@@ -1,9 +1,11 @@
 from libro import Libro
+
 class Usuario:
     
     def __init__(self, rol):
         self.rol = rol
         self.libros_inventario = {}
+        self.libros_prestados= []
         #self.sistema=sistema
         
     
@@ -27,83 +29,37 @@ class Usuario:
         else:
             return "Libro no existe en el sistema"
         
-    def menu_adm(self):
         
-        while True:
-            print("Elija la opcion que desee ejecutar")
-            print("\n1) Agregar libro a inventario")
-            print("2) Buscar libro")
-            print("3) Regresar a iniciar sesion")
-            opcion = input("Seleccione una opción (1, 2) o 'q' para salir: ")
-            
-            if opcion=="1":
-                print("Ingrese codigo del libro ")
-                codigo= str(input(''))
-                print("Ingrese titulo del libro ")
-                titulo= str(input(''))
-                print("Ingrese autor del libro ")
-                autor= str(input(''))
-                libro= Libro(codigo, titulo, autor)
-                self.agregar_libro(libro)
-            
-            elif opcion=="2":
-                print("Ingrese codigo del libro a buscar")
-                codigo= str(input(''))
-                self.buscar_libro(libro.codigo)
-            
-            elif opcion.lower() == 'q':
-                break                
-            else:
-                print("Elija una opcion valida")
-        
-    
-    #Metodos para estudiante
-    
-    def menu_estudiante(self):
-        while True:
-            print("\n1. Ver libros disponibles")
-            print("2. Ver libros prestados")
-            print("3. Hacer préstamo")
-            print("4) Regresar a iniciar sesion")
-            opcion = input("Seleccione una opción (1,2,3,4) o 'q' para salir del programa: ")
 
-            if opcion == '1':
-                self.ver_libros_disponibles()
-            elif opcion == '2':
-                self.ver_libros_prestados()
-            elif opcion == '3':
-                codigo_libro = input("Ingrese el código del libro que desea prestar: ")
-                fecha_prestamo = input("Ingrese la fecha de préstamo: ")
-                fecha_devolucion = input("Ingrese la fecha de devolución: ")
-                self.hacer_prestamo(codigo_libro, fecha_prestamo, fecha_devolucion)
-            #elif opcion == '4':
-            #    self.ver_libros_prestados()
-            elif opcion.lower() == 'q':
-                break
-            else:
-                print("Opción inválida. Intente de nuevo.")
-
-    
-    def prestar(self,libro):
+    def hacer_prestamo(self,codigo, fecha_prestamo, inventario):
         self.prestado: True
-        if libro.codigo in self.libros_inventario:
-            self.libros_inventario[libro.codigo]['cantidad_disponible'] -= 1
-            #if libros_inventario[libro.codigo]['cantidad_disponible']==0:
-                #print("No hay copias disponibles de ese libro")
-        print('Su préstamo fue un éxito')
+        self.fecha_prestamo=fecha_prestamo
+        self.inventario=inventario
+        if codigo in self.inventario:
+            self.inventario[codigo]['cantidad_disponible'] -= 1
+            if self.inventario[codigo]['cantidad_disponible']==0:
+                print("No hay copias disponibles de ese libro")
+            else:
+                self.libros_prestados.append( self.inventario[codigo])
+                print("""Su préstamo fue un éxito
+                      Su fecha de entrega es el: {self.fecha_prestamo}
+                      """)
 
         
-    def devolver(self, libro):
+    def devolver(self, libro,inventario):
         self.prestado: False
         if libro.codigo in self.libros_inventario:
-            self.libros_inventario[libro.codigo]['cantidad_disponible'] += 1
+            self.inventario[libro.codigo]['cantidad_disponible'] += 1
         print('Su libro ha sido devuelto')
         
         
-    def ver_libros_disponibles(self):
+    def ver_libros_disponibles(self,inventario):
+        self.inventario=inventario
         print("Libros disponibles en el sistema: ")
-        print(self.libros_inventario)
+        print(self.inventario)
         
-       
+    def ver_libros_prestados(self):
+        print("Historial de libros prestados: ")
+        print(self.libros_prestados)
    
     
